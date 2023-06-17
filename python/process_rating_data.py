b626401c900822dff42f1c20ab164cfde4369620
mginsburg2578 <mark.ginsburg@unifyconsulting.com>
2023-06-17 09:04:00 -0700

    add argument for the DateStamp

--- python/process_rating_data.py
+++ python/process_rating_data.py
@@ -1,15 +1,20 @@
 
-''' process_ratings.py RATING_CUTOFF,BASEFILENAME 
+''' process_ratings.py RATING_CUTOFF,BASEFILENAME ,DICTLABEL
     
     For the time being it reads in a FIDE Dataset, 
     BASEFILENAME.dat 
     pulls out the Rating and Nationality, for only players
-    whose ratings are >= RATING
+    whose ratings are >= RATING_CUTOFF
     It loads those into an output flat file.  BASEFILENAME.out 
     Then, a dataframe is constructed called df.a  We add a small dict (assign) to df in order to represent the MMYYYY
-    timestamp for this dataset as a new column, in this case '102001'.
+    This is derived from the DICTLABEL argument, for example '102001' or '012002' 
     In the final step, a df2 dataframe counts the # of 2600-plus-players per nation and timestamp via groupby and the size() function.
     The df2 is written out to disk.   BASEFILENAMEfinal.out 
+    
+    Usage:  first get a FIDE file from https://ratings.fide.com/download/feb01frl.zip and extract it and rename it to 
+    MMMYYYY.dat   then for example issue this:
+    python ./process_ratings.py 2600 apr2002 042002 
+    The program bypasses the string header row.
 
     MG  June 2023
 
@@ -20,11 +25,13 @@
 
 import re,sys 
 import pandas as pd 
-if len(sys.argv) != 3:
-    print("Usage", sys.argv[0] , "ratingcutoff,basefilename")
+if len(sys.argv) != 4:
+    print("Usage", sys.argv[0] , "ratingcutoff,basefilename,dictlabel")
     sys.exit(1) 
 rating_cutoff = int(sys.argv[1]) 
 basefilename =  sys.argv[2] 
+dictlabel    =  sys.argv[3] 
+
 infile = 'C:/Users/Elite/chess/panel_app_chess/panel_app_chess/data/' + basefilename + '.dat'
 
 ctr = 0 
@@ -46,7 +53,7 @@ with open(outfile, 'w') as out:
                     sys.exit(0)
             else:
                 print("Do not process header.") 
-ds = {'ds': '102001'}  # datestamp column label and value,  oct 2001 data source 
+ds = {'ds': dictlabel }  # datestamp column label and value,  oct 2001 data source 
 data = pd.read_csv(outfile,encoding="ISO-8859-1",sep='\t',header=None,names=["rtg","nat"])
 data = data.assign(**ds) 
 data = data[['ds','nat','rtg']]
