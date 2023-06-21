a054b99170382c76970e1905c077ff5733f40ae3
mginsburg2578 <mark.ginsburg@unifyconsulting.com>
2023-06-21 15:04:29 -0700

    year 2018 and 2019

--- data/full/fide.bat
+++ data/full/fide.bat
@@ -1,11 +1,21 @@
-wget https://ratings.fide.com/download/jan11frl.zip
-wget https://ratings.fide.com/download/mar11frl.zip
-wget https://ratings.fide.com/download/jul11frl.zip
-wget https://ratings.fide.com/download/nov11frl.zip
+wget https://ratings.fide.com/download/standard_jan18frl.zip
+wget https://ratings.fide.com/download/standard_apr18frl.zip
+wget https://ratings.fide.com/download/standard_jul18frl.zip
+wget https://ratings.fide.com/download/standard_oct18frl.zip
 
-tar -xf jan11frl.zip
-tar -xf mar11frl.zip
-tar -xf jul11frl.zip
-tar -xf nov11frl.zip
+tar -xf standard_jan18frl.zip
+tar -xf standard_apr18frl.zip
+tar -xf standard_jul18frl.zip
+tar -xf standard_oct18frl.zip
 
+
+wget https://ratings.fide.com/download/standard_jan19frl.zip
+wget https://ratings.fide.com/download/standard_apr19frl.zip
+wget https://ratings.fide.com/download/standard_jul19frl.zip
+wget https://ratings.fide.com/download/standard_oct19frl.zip
+
+tar -xf standard_jan19frl.zip
+tar -xf standard_apr19frl.zip
+tar -xf standard_jul19frl.zip
+tar -xf standard_oct19frl.zip
 
